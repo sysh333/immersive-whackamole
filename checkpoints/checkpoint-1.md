dad71823dddf5bb0489ee08eafe5af89fe946b40
Ryosuke Kawamura <clonable.eden@gmail.com>
2018-05-14 20:33:56 +0900

    checkpoint-0 complete

---
+++ checkpoints/checkpoint-1.md
@@ -0,0 +1,14 @@
+# Checkpoint 1
+
+**Live Coding!** In this checkpoint, watch a tech mentor live code the following:
+
+* [ ] Create Moles component
+* [ ] Include Moles in the App component using the `components` VueJS property
+  * [ ] Learn about the `import` syntax
+* [ ] Render the Moles in the App template
+* [ ] Pass the moles data array as a “prop” into the Moles component
+  * [ ] V-bind directive
+  * [ ] `props` VueJS property
+* [ ] Loop over the data array to create multiple DOM elements
+  * [ ] `v-for` directive
+  * [ ] `v-bind:key` for binding a data property to props
--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,64 @@
 <template>
-  
+  <div class="whackamole">
+    <h1 class="logo">
+      Whackamole
+    </h1>
+    <button 
+      class="start-game"
+      v-on:click="gameStart"
+    >
+      Start Game
+    </button>
+    <div class="counters-container">
+      <div class="counter">
+        <h1>Score:</h1>
+        <h2>{{ score }}</h2>
+      </div>
+      <div class="counter">
+        <h1>High Score:</h1>
+        <h2>{{ highScore }}</h2>
+      </div>
+      <div class="counter">
+        <h1>Timer:</h1>
+        <h2>{{ timer }}</h2>
+      </div>
+    </div>
+    <div class="moles-container">
+      <div class="mole">{{ moles[0] }}</div>
+      <div class="mole">{{ moles[1] }}</div>
+      <div class="mole">{{ moles[2] }}</div>
+      <div class="mole">{{ moles[3] }}</div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  
+  name: 'App',
+  data: () => {
+    return {
+      score: 0,
+      highScore: 10,
+      timer: 20,
+      moles: [false, false, false, false]
+    };
+  },
+  methods: {
+    gameStart: function() {
+      console.log('I was clicked!');
+    }
+  }
 }
 </script>
 
 <style>
+.counter {
+  margin: 10px;
+  display: inline-block;
+}
 
+.mole {
+  margin: 10px;
+  display: inline-block;
+}
 </style>
